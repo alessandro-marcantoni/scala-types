06cb8c5e95af57c10523e5daf3b78ea8824b1c89
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2022-08-10 01:31:23 +0200

    ci: update codecov

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,16 @@
 module.exports = {
-  collectCoverage: true,
-  coverageThreshold: {
-    global: {
-      lines: 90
-    }
-  },
-  transform: {'^.+\\.ts?$': 'ts-jest'},
-  testEnvironment: 'node',
-  testRegex: '/src/test/.*\\.(test|spec)?\\.(ts|tsx)$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+    collectCoverage: true,
+    coverageReporters: [
+        "text",
+        "cobertura",
+    ],
+    coverageThreshold: {
+        global: {
+            lines: 90
+        }
+    },
+    transform: {"^.+\\.ts?$": "ts-jest"},
+    testEnvironment: "node",
+    testRegex: "/src/test/.*\\.(test|spec)?\\.(ts|tsx)$",
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
 };
